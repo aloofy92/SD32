98d292152979294c0e39e67b220e748aed000f0f
Brianna Hickerson <brehickerson@yahoo.com>
2022-03-03 08:48:40 -0800

    quiz2

---
+++ Java/workspace/Quiz2/src/PersonPlay.java
@@ -0,0 +1,52 @@
+class Person {
+// 1. TODO: privat complete the Person class given here
+// 1. TODO: Give the Person well encapsulated fields for their first and last names
+
+// 2. TODO: create a constructor for Person
+private firstName;
+private lastName;
+int age;
+}
+
+public class PersonPlay {
+
+  public static void main(String[] args) {
+	  Personplay2();
+  }
+    // 1. TODO:  Complete the Person class using good encapsulation.
+
+    // 3. TODO:  Construct three Person objects and assign each Person a first and
+	  // 4. TODO:  Assign each of the three Person objects into an array.
+	  
+	  // 5. TODO:  Iterate through the array and display each Person's first and last
+	  //        name.
+    //        last name.
+	  public Personplay2(String firstName, String lastName, int age) {
+		    this.firstName = firstName;
+		    this.lastName = lastName;
+		    this.age = age;
+
+
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+Message sd32
+
+
+
+
+
+
+
+
---
+++ Java/workspace/Quiz2/src/Personplay2.java
@@ -0,0 +1,23 @@
+
+public class Personplay2 extends human {
+	private String firstName;
+	private String lastName;
+	private int age;
+	
+	
+	  public Personplay2( ) { }
+	  
+	  public Personplay2(
+			  String fn, String ln, int a)
+	  {
+	this.lastName = ln;
+	age = a;
+	this.firstName = fn;
+	
+	
+	
+	public String getInfo() {
+	    return getName() + " " + age + " " + title + " " ;
+	  }
+
+}
